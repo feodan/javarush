ad2d067c068f9eec7a12603faabe3ee724da9cbc
afedorov <feodan@yandex.ru>
2016-11-15 18:12:39 +0200

    11/15/2016

--- src/com/javarush/test/level03/lesson08/task01/Solution.java
+++ src/com/javarush/test/level03/lesson08/task01/Solution.java
@@ -14,7 +14,10 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        String sAge = reader.readLine();
+        int age = Integer.parseInt(sAge);
 
     }
 }
