95430b53d68e6e55b434c89102d7d5325a5958ca
afedorov <feodan@yandex.ru>
2016-12-28 18:29:55 +0200

    28/12/2016

---
+++ src/com/javarush/my1/Date.java
@@ -0,0 +1,8 @@
+package com.javarush.my1;
+
+public class Date {
+    public static void main(String[] args) throws Exception {
+        Date beforeTime = new Date();
+        System.out.println(System.currentTimeMillis());
+    }
+}
---
+++ src/com/javarush/test/level08/lesson06/task01/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level08.lesson06.task01;
+
+/* Создать два списка LinkedList и ArrayList
+Нужно создать два списка – LinkedList и ArrayList.
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public static Object createArrayList() {
+        List list = new ArrayList();
+        return list;
+    }
+
+    public static Object createLinkedList() {
+        List list = new LinkedList();
+        return list;
+    }
+}
