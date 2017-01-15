d379251dd255fc208493d956937a360f623558b6
feodan <feodan@yandex.ru>
2017-01-16 00:15:25 +0200

    161/2017

--- src/com/javarush/test/level09/lesson02/task02/Solution.java
+++ src/com/javarush/test/level09/lesson02/task02/Solution.java
@@ -1,7 +1,8 @@
 package com.javarush.test.level09.lesson02.task02;
 
 /* И снова StackTrace
-Написать пять методов, которые вызывают друг друга. Каждый метод должен возвращать имя метода, вызвавшего его, полученное с помощью StackTrace.
+Написать пять методов, которые вызывают друг друга.
+Каждый метод должен возвращать имя метода, вызвавшего его, полученное с помощью StackTrace.
 */
 
 public class Solution {
@@ -11,30 +12,53 @@ public class Solution {
 
     public static String method1() {
         method2();
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        return Thread.currentThread().getStackTrace().getMethodName();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method2() {
         method3();
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        return stackTraceElements[0].getMethodName();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method3() {
         method4();
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        return stackTraceElements[0].getMethodName();
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method4() {
         method5();
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        return stackTraceElements[0].getMethodName();
+        System.out.println(Thread.currentThread().getStackTrace()[0].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[3].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[4].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[5].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace().length);
+        System.out.println();
+
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 
     public static String method5() {
-        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        return stackTraceElements[0].getMethodName();
+//        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+//        for (StackTraceElement element : stackTraceElements) {
+//            System.out.println(element.getMethodName());
+//        }
+        System.out.println(Thread.currentThread().getStackTrace()[0].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[3].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[4].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[5].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace()[6].getMethodName());
+        System.out.println(Thread.currentThread().getStackTrace().length);
+        System.out.println();
+
+        System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName());
+        return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 }
