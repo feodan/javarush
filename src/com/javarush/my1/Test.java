d3114c9bc6340e08b08e3fad3e17ac35bb1aa077
feodan <feodan@yandex.ru>
2017-01-25 00:36:31 +0200

    25/1/2017

--- src/com/javarush/my1/Test.java
+++ src/com/javarush/my1/Test.java
@@ -2,8 +2,33 @@ package com.javarush.my1;
 
 public class Test {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 7;
-        System.out.println((a * 1.0) / b);
+        char ch = 'S';
+
+        // c использованием класса Character
+        String charToString = Character.toString(ch);
+        System.out.println(charToString);
+
+        // с использованием операции добавления класса String
+        String str = "" + ch;
+        System.out.println(str);
+
+        //с использованием массива
+        String fromChar = new String(new char[] { ch });
+        System.out.println(fromChar);
+
+        // с использованием метода valueOf класса String
+        String valueOfchar = String.valueOf(ch);
+        System.out.println(valueOfchar);
+
+        char ch1 = '9';
+
+        // c использованием метода getNumericValue
+        // класса Character
+        int i1 = Character.getNumericValue(ch1);
+        System.out.println(i1);
+
+        // c использованием метода digit класса Character
+        int i2 = Character.digit(ch1,10);
+        System.out.println(i2);
     }
 }
